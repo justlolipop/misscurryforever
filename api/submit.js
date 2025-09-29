f459f1485ac535e276cc2fe241b97b1708ef476e
justlolipop <justlolipopks@gmail.com>
2025-09-29 12:03:27 +0800

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,20 +1,24 @@
-export async function POST(req, res) {
-  let body = "";
-
-  req.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-
-  req.on("end", () => {
-    const params = new URLSearchParams(body);
-    const name = params.get("name");
-    const email = params.get("email");
-    const number = params.get("number");
-    const wish = params.get("wish");
-
-    res.status(200).json({
-      message: "Form submitted successfully!",
-      data: { name, email, number, wish },
+module.exports = (req, res) => {
+  if (req.method === "POST") {
+    let body = "";
+    
+    req.on("data", (chunk) => {
+      body += chunk.toString();
     });
-  });
-}
+    
+    req.on("end", () => {
+      const params = new URLSearchParams(body);
+      const name = params.get("name");
+      const email = params.get("email");
+      const number = params.get("number");
+      const wish = params.get("wish");
+      
+      res.status(200).json({
+        message: "Form submitted successfully!",
+        data: { name, email, number, wish },
+      });
+    });
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+};
